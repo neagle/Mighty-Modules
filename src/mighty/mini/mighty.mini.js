bcea70798ad27e60ebf2245d438f6bcb4dcd80c1
Nate Eagle <n.eagle@gmail.com>
2013-12-18 01:47:48 -0500

    create cards from json feed

--- src/mighty/mini/mighty.mini.js
+++ src/mighty/mini/mighty.mini.js
@@ -21,63 +21,11 @@ Mighty.define(['mighty.core', 'mighty/mini/mighty.mini.css'], function (core) {
 		// Set up the widget
 		_create: function () {
 
-			var self = this,
-				options = self.options,
-				ui = self.ui,
-				element = self.element,
+			var self = this;
+			var options = self.options;
+			var ui = self.ui;
+			var element = self.element;
 
-				articles = ui.articles,
-				photos = ui.photos,
-
-				i,
-				l,
-
-				showMore,
-
-				count = parseInt(options.count, 10),
-				moreCount = parseInt(options.more_count, 10);
-
-			// If our count is greater than the number of articles,
-			// reduce the count.
-			if (count > articles.length) {
-				count = articles.length;
-			}
-
-			function moreStories(event) {
-				for (i = 0; i < moreCount; i += 1) {
-					count += 1;
-					if (count === l) {
-						showMore.style.display = 'none';
-						break;
-					} else {
-						articles[count].style.display = 'block';
-						photos[count].src = core.data(photos[count], 'src');
-					}
-				}
-				event.preventDefault();
-			}
-
-			// Display the photos for the visible stories.
-			for (i = 0, l = count; i < l; i += 1) {
-				photos[i].src = core.data(photos[i], 'src');
-			}
-
-			// If count is specified, hide stories.
-			if (articles[count]) {
-				for (i = count, l = articles.length; i < l; i += 1) {
-					articles[i].style.display = 'none';
-				}
-
-				// Create showMore link
-				showMore = document.createElement('a');
-				showMore.innerHTML = 'More Stories ▾';
-				showMore.className = 'show-more';
-				showMore.href = '#';
-
-				element.appendChild(showMore);
-
-				core.bind(showMore, 'click', moreStories);
-			}
 		}
 	};
 });
--- src/mighty/mini/mighty.mini.php
+++ src/mighty/mini/mighty.mini.php
@@ -1,15 +1,18 @@
 <?php
 /*
-    Mighty Most Popular Module
-    Displays the most popular content from across the AOL network.
+		Mighty Mini Cards
+		This module displays a feed of cards from a selected brand.
 
-    Options:
-        heading
-        sub_hedaing
-        source
-        sort
-        count
-*/
+		Options:
+				heading
+				sub_hedaing
+				source
+				sort
+				count
+ */
+
+date_default_timezone_set('UTC');
+
 if (isset($options->heading)): $heading = '<h2>' . $options->heading . '</h2>'; endif;
 if (isset($options->sub_heading)): $sub_heading = '<h3>' . $options->sub_heading . '</h3>'; endif;
 
@@ -19,34 +22,83 @@ if (isset($options->sort) && $options->sort === 'viral'): $sort = '&viral_sort=1
 
 // Call API
 $Mighty = new Mighty();
-$json = $Mighty->getJSON('http://www.huffingtonpost.com/api/?t=most_popular_merged' . $sort);
+$json = $Mighty->getJSON('http://qa.mini.aol.com/api/1.0/cards');
 
 if (isset($json)):
 
-$articles = $json->response;
-// print_r($articles);
+		$cards = $json->data->cards;
 
-if (count($articles) > 0):
+if (count($cards) > 0):
 ?>
-<div class="mighty-mostpopular">
-    <?=@$heading?>
-    <?=@$sub_heading?>
-    <ol>
-<?  foreach ($articles as $article):
+<div class="mighty-mini">
+		<div class="cards-list">
+		<?=@$heading?>
+		<?=@$sub_heading?>
 
-        // Title falls back to entry title.
-        $title = $article->entry_front_page_title;
-        if (!$title): $title = $article->entry_title; endif;
+<? foreach ($cards as $card):
+	$type_class = "card-type-" . $card->card_type->name;
+	$source = $card->source;
 
-        // If source option is set, show source site.
-        if (isset($options->source)): $source = '<i><a href="' . $article->vertical_link . '">' . ($article->source === 'HuffPo' ? 'HuffPost ' : '') . $article->vertical_name . '</a></i>'; endif;
-?>      <li>
-        <a href="<?=@$article->entry_url?>"><img width="74" height="58" alt="<?=@$article->entry_image_keywords?>" src="<?=$Mighty->option("basePath").'images/x.gif'?>" data-src="<?=@$article->entry_image?>"></a>
-        <?=@$source?>
-        <b><a href="<?=@$article->entry_url?>"><?=@$title?></a></b>
-        </li>
-<?  endforeach; ?>
-    </ol>
+	$updated = date_create();
+	date_timestamp_set($updated, $card->updated);
+
+	$now = date_create();
+	$diff = date_diff($updated, $now);
+	$ago = $diff->format("%h") . "h";
+?>
+
+		<article class="card card-list <?=$type_class?>">
+
+			<? switch ($card->card_type->name) {
+			case "headline": ?>
+
+			<? break; ?>
+			<? case "quote": ?>
+
+			<? break; ?>
+			<? case "video": ?>
+
+			<div class="card-video-poster" style="background-image:url('<?=$card->content->media[1]->url?>')">
+				<div class="card-play"><i class="icon-play"></i></div>
+			</div>
+
+			<? break; ?>
+			<? case "image": ?>
+
+			<div class="card-image" style="background-image:url('<?=$card->media[0]['url']?>');"></div>
+
+			<? break; ?>
+			<? } ?>
+
+			<? if ($card->state) { ?>
+			<h4 class="card-state card-state-<?=$card->state?>"><?=ucfirst($card->state)?></h4>
+			<? } ?>
+
+			<h2 class="headline"><?=$card->content->text?></h2>
+			<p class="card-comment"><?=$card->content->comment?></p>
+
+			<p class="card-meta">
+				<span class="card-icon pull-left">
+					<img src="http://pbs.twimg.com/profile_images/378800000833708794/2e214d2fde9f61190b1ffa5d601d762c_normal.png">
+				</span>
+
+				<span class="card-meta-content">
+					<span class="card-author-name"><?=$card->author->display_name?>
+						<? if ($source->name) { ?>
+							via <span class="card-source-name"><?=$source->name?></span>
+						<? } ?>
+					</span>
+				</span>
+
+				<span class="card-meta-end">
+					<span class="card-ago"><?=$ago?></span>
+				</span>
+			</p>
+
+		</article>
+		<?  endforeach; ?>
+
+		</div>
 </div>
 <?php
 endif;
