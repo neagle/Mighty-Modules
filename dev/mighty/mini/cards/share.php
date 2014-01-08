bb15301ffbd084357d3d3c53fa76f379290a8fd0
Nate Eagle <n.eagle@gmail.com>
2014-01-08 12:01:02 -0500

    add share bar

---
+++ dev/mighty/mini/cards/share.php
@@ -0,0 +1,11 @@
+<div class="card-meta-share">
+	<ul>
+		<li class="share facebook">Facebook</li>
+		<li class="share twitter">Twitter</li>
+		<? /* <li class="share email">Email<li> */ ?>
+		<li class="share googleplus">Google Plus</li>
+		<li class="share pinterest">Pinterest</li>
+		<li class="share stumbleupon">Stumble Upon</li>
+		<li class="share reddit">Reddit</li>
+	</ul>
+</div>
